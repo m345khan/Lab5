fc7b32be5ac19f0f5ef92f466181863953b4afc9
m345khan <malik.khan@ryerson.ca>
2015-10-26 16:16:11 -0400

    extra line

--- Lab5/src/Lab5.cpp
+++ Lab5/src/Lab5.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout<<"My name is Malik"
-    cout<<"HI";
+	cout<<"WOrk man";
+	cout<<"dammm";
 	return 0;
 }
